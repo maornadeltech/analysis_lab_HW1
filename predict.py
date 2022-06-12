0b346b1b5e58efebd5e30f0ac0a5ef1acdda9a59
maornadeltech <86251877+maornadeltech@users.noreply.github.com>
2022-06-12 22:03:25 +0300

    fixed the predict script

--- predict.py
+++ predict.py
@@ -3,17 +3,24 @@ import os
 import pandas as pd
 import pickle
 import data_loader
-model_path = '/home/student/data/model.pkl'
-imputer_path = '/home/student/data/imputer.pkl'
+import numpy as np
+import tqdm
+from sklearn.metrics import f1_score
+
+model_path = 'model.pkl'
+imputer_path = 'imputer.pkl'
+batch_size = 1000
 
 data_path = sys.argv[1]
 model = pickle.load(open(model_path, 'rb'))
 imputer = pickle.load(open(imputer_path, 'rb'))
 res = {'Ids': [], 'SepsisLabel': []}
+vecs = []
+reals = []
 
-for path in os.listdir(f'{data_path}'):
+for path in tqdm.tqdm(os.listdir(data_path)):
     p_id = path.split("/")[-1].split(".")[-2].split('_')[-1]
-    p_df = pd.read_csv(path, sep='|').interpolate(limit_direction='both', axis=0)
+    p_df = pd.read_csv(f"{data_path}/{path}", sep='|').interpolate(limit_direction='both', axis=0)
     label = p_df['SepsisLabel'].max()
     idx = 0
     if label:
@@ -23,8 +30,16 @@ for path in os.listdir(f'{data_path}'):
     p_df = p_df.drop(['SepsisLabel', 'Unit1', 'Unit2', 'ICULOS'], axis=1)
     values = imputer.transform(p_df.values)
     p_df = pd.DataFrame(values.round(2), columns=p_df.columns)
-    p_vec = data_loader.get_patient_vec(p_df, idx)
+    vecs.append(data_loader.get_patient_vec(p_df, idx))
+    if len(vecs) == batch_size:
+        res['SepsisLabel'].extend(model.predict(np.stack(vecs)).tolist())
+        vecs = []
     res['Ids'].append(p_id)
-    res['SepsisLabel'].append(model.predict(vec))
+    reals.append(label)
+
+if vecs:
+    res['SepsisLabel'].extend(model.predict(np.stack(vecs)).tolist())
+
+print(f"f1 = {f1_score(np.asarray(reals), np.asarray(res['SepsisLabel']))}")
 
 pd.DataFrame.from_dict(res).to_csv('prediction.csv', index=False)
